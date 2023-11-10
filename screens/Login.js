fcb3ce2bf1d0837be9128d058d121ca703adb243
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2023-11-10 22:48:34 +1100

    Added navigator, auth pages-without functionality

---
+++ screens/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+const Login = ({ route, navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        margin: "10%",
+      }}>
+      <View style={{ alignItems: "center" }}>
+        <Text>Log In</Text>
+      </View>
+      <View>
+        <TextInput
+          theme={{ roundness: 25 }}
+          style={{
+            overflow: "hidden",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: 25,
+          }}
+          label='Email'
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput
+          theme={{ roundness: 25 }}
+          style={{
+            overflow: "hidden",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: 25,
+          }}
+          label='Password'
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+          secureTextEntry={true}
+        />
+      </View>
+      <View>
+        <Button
+          mode='contained'
+          onPress={() =>
+            navigation.navigate("Home", {
+              method: "login",
+              email: email,
+              password: password,
+            })
+          }>
+          Log In
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
---
+++ screens/Signup.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+const Signup = ({ route, navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        margin: "10%",
+      }}>
+      <View style={{ alignItems: "center" }}>
+        <Text>Create an Account</Text>
+      </View>
+      <View>
+        <TextInput
+          theme={{ roundness: 25 }}
+          style={{
+            overflow: "hidden",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: 25,
+          }}
+          label='Name'
+          value={name}
+          onChangeText={(name) => setName(name)}
+        />
+        <TextInput
+          theme={{ roundness: 25 }}
+          style={{
+            overflow: "hidden",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: 25,
+          }}
+          label='Email'
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput
+          theme={{ roundness: 25 }}
+          style={{
+            overflow: "hidden",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderRadius: 25,
+          }}
+          label='Password'
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+          secureTextEntry={true}
+        />
+      </View>
+      <View>
+        <Button
+          mode='contained'
+          onPress={() =>
+            navigation.navigate("Home", {
+              method: "signup",
+              name: name,
+              email: email,
+              password: password,
+            })
+          }>
+          Sign up
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default Signup;
