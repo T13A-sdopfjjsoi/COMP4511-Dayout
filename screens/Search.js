bc1b09e131f101c4d1b040c98841be6987621e06
Jared Goodall <jaredgoodall@Jareds-MacBook-Air.local>
2023-11-14 13:02:24 +1100

    Nav to events

--- screens/Search.js
+++ screens/Search.js
@@ -2,8 +2,11 @@ import React from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Searchbar, Avatar, Button, Card, Title, Paragraph, IconButton  } from 'react-native-paper';
 import UIStyles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 const Search = () => {
+  const navigation = useNavigation();
+
   const [searchQuery, setSearchQuery] = React.useState('');
 
   const onChangeSearch = query => setSearchQuery(query);
@@ -20,7 +23,8 @@ const Search = () => {
       <ScrollView style={{width:"100%", paddingLeft: 20, paddingRight: 20, paddingBottom: 20}}>
         <Text>{searchQuery}</Text>
         {events.map((event) => (
-          <Card key={event} style={UIStyles.searchCard}>
+          <Card key={event} style={UIStyles.searchCard} 
+          onPress={() => navigation.navigate('Event', { eventId: event })}>
             <Card.Content>
               <Title>Event title here = {event}</Title>
               <Paragraph>lorem ipsum and shit lorem ipsum and shit lorem ipsum and shit lorem ipsum and shit lorem ipsum and shit</Paragraph>
