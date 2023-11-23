6c8c95f968e8c06f0825846e89d588861bd1058f
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2023-11-23 22:08:57 +1100

    refactor social screen code

---
+++ screens/Components/SocialTopBar.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import UIStyles from "../styles.js";
+
+const SocialTopBar = ({ ActiveUser }) => {
+  const navigation = useNavigation();
+  return (
+    <>
+      {!ActiveUser?.username ? (
+        <View
+          style={{
+            paddingTop: 60,
+            padding: 20,
+            width: "100%",
+          }}>
+          <LinearGradient
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            colors={["#9f78f0", "#9d76bb"]}>
+            <Text
+              style={{ fontSize: 30, color: "#ffffff", fontWeight: "bold" }}>
+              Welcome!
+            </Text>
+            <View style={UIStyles.buttonContainer}>
+              <Button
+                mode='contained'
+                title='Login'
+                style={UIStyles.button}
+                onPress={() => {
+                  navigation.navigate("Login");
+                }}>
+                Log in
+              </Button>
+              <Text style={UIStyles.titleText}>or </Text>
+              <Button
+                mode='contained'
+                title='Sign up'
+                style={UIStyles.button}
+                onPress={() => {
+                  navigation.navigate("Signup");
+                }}>
+                Sign up
+              </Button>
+            </View>
+          </LinearGradient>
+        </View>
+      ) : (
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={["#9f78f0", "#9d76bb"]}>
+          <View
+            style={{
+              paddingTop: 60,
+              padding: 20,
+            }}>
+            <Text
+              style={{ fontSize: 30, color: "#ffffff", fontWeight: "bold" }}>
+              Social Your Way
+            </Text>
+            <Text
+              style={{
+                fontSize: 30,
+                color: "#ffffff",
+                textDecorationLine: "underline",
+              }}>
+              {ActiveUser.username}
+            </Text>
+          </View>
+        </LinearGradient>
+      )}
+    </>
+  );
+};
+
+export default SocialTopBar;
