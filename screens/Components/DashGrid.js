540b0eb0d4e28a9a1cdb165ec351275ea416e0c7
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2023-11-24 00:23:33 +1100

    update hook method

--- screens/Components/DashGrid.js
+++ screens/Components/DashGrid.js
@@ -1,164 +1,265 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { ScrollView, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import { ScrollView, View, Image, TouchableOpacity } from "react-native";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
-import { Button, Card, Text, Title, Paragraph } from 'react-native-paper';
-import UIStyles from '../styles';
-import StoreService from '../../services/StoreService';
+import { Button, Card, Text, Title, Paragraph } from "react-native-paper";
+import UIStyles from "../styles";
+import StoreService from "../../services/StoreService";
 import { format } from "date-fns";
 
 const DashGrid = () => {
   const navigation = useNavigation();
-  const events = [1,2,3,4,5,6];
+  const events = [1, 2, 3, 4, 5, 6];
   const [storedEvents, setStoredEvents] = useState([]);
-  const [interested, setInterested] = useState([])
-  const [shuffled, setShuffled] = useState([])
+  const [interested, setInterested] = useState([]);
+  const [shuffled, setShuffled] = useState([]);
 
-  const [joined, setJoined] = useState([])
-  const [user, setUser] = useState('')
+  const [joined, setJoined] = useState([]);
+  const [user, setUser] = useState("");
 
-  useEffect(() => { 
-    const fetchUser = async () => {
-      const user = await StoreService.getActive();
-      setUser(user);
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const user = await StoreService.getActive();
+  //     setUser(user);
 
-      await getStoredEvents();
-    };
-    fetchUser();
-  }, []);
+  //     await getStoredEvents();
+  //   };
+  //   fetchUser();
+  // }, []);
 
+  useFocusEffect(
+    useCallback(() => {
+      const fetchUser = async () => {
+        const user = await StoreService.getActive();
+        setUser(user);
+
+        await getStoredEvents();
+      };
+      fetchUser();
+    }, [])
+  );
 
   const getStoredEvents = async () => {
     const storedEvents = await StoreService.getEvents();
     setStoredEvents(storedEvents);
     //https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
-    setInterested(storedEvents.sort((a, b) => 0.5 - Math.random()))
-    setShuffled(storedEvents.sort((a, b) => 0.5 - Math.random()))
-    setJoined(storedEvents.sort((a, b) => 0.5 - Math.random()))
+    setInterested(storedEvents.sort((a, b) => 0.5 - Math.random()));
+    setShuffled(storedEvents.sort((a, b) => 0.5 - Math.random()));
+    setJoined(storedEvents.sort((a, b) => 0.5 - Math.random()));
     return storedEvents;
-  }
+  };
 
-
-  return (
-    (user ? (
-      <ScrollView style={{ width: '100%', height:"100%", marginBottom: 10 }}>
-        <Text style={{fontWeight:"bold"}}>For you</Text>
-        <ScrollView horizontal={true} contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
-          {storedEvents.map((event) => (
-            <TouchableOpacity key={event.id} onPress={() => navigation.navigate('Event', { eventId: event.id })}>
-            <Card style={{width: 120, height: 180, margin:5,}} onPress={()=>navigation.navigate("Event", {eventId: event.id })}>
-            <Card.Cover style={{height : 110}} source={{ uri: event.image }} />
-            <Card.Content>
-              <Title numberOfLines={1} style={{fontSize:14}}>{event.name}</Title>
-              <Paragraph numberOfLines={1} style={{fontSize:12}}>{format(new Date(event.date), "do/MMM")}</Paragraph>
-            </Card.Content>
-          </Card>
+  return user ? (
+    <ScrollView style={{ width: "100%", height: "100%", marginBottom: 10 }}>
+      <Text style={{ fontWeight: "bold" }}>For you</Text>
+      <ScrollView
+        horizontal={true}
+        contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
+        {storedEvents.map((event) => (
+          <TouchableOpacity
+            key={event.id}
+            onPress={() => navigation.navigate("Event", { eventId: event.id })}>
+            <Card
+              style={{ width: 120, height: 180, margin: 5 }}
+              onPress={() =>
+                navigation.navigate("Event", { eventId: event.id })
+              }>
+              <Card.Cover
+                style={{ height: 110 }}
+                source={{ uri: event.image }}
+              />
+              <Card.Content>
+                <Title numberOfLines={1} style={{ fontSize: 14 }}>
+                  {event.name}
+                </Title>
+                <Paragraph numberOfLines={1} style={{ fontSize: 12 }}>
+                  {format(new Date(event.date), "do/MMM")}
+                </Paragraph>
+              </Card.Content>
+            </Card>
           </TouchableOpacity>
         ))}
         {storedEvents.length === 0 && (
-          <Card key={"suggestions"} style={{justifyContent:"center", alignItems:"center", width: 120, height: 180, margin:5,}}>
-            <Card.Content style={{justifyContent:"center"}}>
-              <Title style={{fontSize:14}}>No Events</Title>
+          <Card
+            key={"suggestions"}
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 120,
+              height: 180,
+              margin: 5,
+            }}>
+            <Card.Content style={{ justifyContent: "center" }}>
+              <Title style={{ fontSize: 14 }}>No Events</Title>
             </Card.Content>
           </Card>
         )}
-        </ScrollView>
-        <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', marginRight: 10 }}>
-          <TouchableOpacity style={{ width: '48%', alignItems: 'center' }} onPress={() => navigation.navigate("Search")}>
-            <Text style={{fontWeight:"bold", marginBottom: 5}}>Interested</Text>
-            <View style={UIStyles.stack}>
-              {(() => {
-                let stack = [];
-                for (let index = 0; index < Math.min(interested.length, 4); index++) {
-                  if (interested[index]?.image) {
-                      stack.push(
-                        <Image
-                          key={"interested" + index}
-                          source={{ uri: interested[index]?.image }}
-                          style={{ width: '50%', height: '50%' }}
-                        />
-                      );
-                    }
-                  }
-
-                if (stack.length === 0) {
-                  stack = ([<Text key="noInterested" style={{padding:10}}>No Interested Events</Text>]);
-                }
-                return stack 
-              })()}
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ width: '48%', alignItems: 'center' }} onPress={() => navigation.navigate("Search")}>
-            <Text style={{fontWeight:"bold", marginBottom: 5}}>Joined</Text>
-            <View style={UIStyles.stack}>
-              {(() => {
-                let stack = [];
-                for (let index = 0; index < Math.min(joined.length, 4); index++) {
-                  if (joined[index]?.image) {
-                      stack.push(
-                        <Image
-                          key={"Joined" + index}
-                          source={{ uri: joined[index]?.image }}
-                          style={{ width: '50%', height: '50%' }}
-                        />
-                      );
-                    }
-                  }
-
-                if (stack.length === 0) {
-                  stack = ([<Text key="noJoined" style={{padding:10}}>No Joined Events</Text>]);
-                }
-                return stack 
-              })()}
-            </View>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
-    ) : (
-      <ScrollView style={{ width: '100%', height:"100%", marginBottom: 10 }}>
-        <Text style={{fontWeight:"bold"}}>For you</Text>
-        <ScrollView horizontal={true} contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
-          {storedEvents.map((event) => (
-            <TouchableOpacity key={event.id + "fy"} onPress={() => navigation.navigate('Event', { eventId: event.id })}>
-            <Card  style={{width: 120, height: 180, margin:5,}} onPress={()=>navigation.navigate("Event", {eventId: event.id })}>
-            <Card.Cover style={{height : 110}} source={{ uri: event.image }} />
-            <Card.Content>
-              <Title numberOfLines={1} style={{fontSize:14}}>{event.name}</Title>
-              <Paragraph numberOfLines={1} style={{fontSize:12}}>{format(new Date(event.date), "do/MMM")}</Paragraph>
-            </Card.Content>
-          </Card>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          marginRight: 10,
+        }}>
+        <TouchableOpacity
+          style={{ width: "48%", alignItems: "center" }}
+          onPress={() => navigation.navigate("Search")}>
+          <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
+            Interested
+          </Text>
+          <View style={UIStyles.stack}>
+            {(() => {
+              let stack = [];
+              for (
+                let index = 0;
+                index < Math.min(interested.length, 4);
+                index++
+              ) {
+                if (interested[index]?.image) {
+                  stack.push(
+                    <Image
+                      key={"interested" + index}
+                      source={{ uri: interested[index]?.image }}
+                      style={{ width: "50%", height: "50%" }}
+                    />
+                  );
+                }
+              }
+
+              if (stack.length === 0) {
+                stack = [
+                  <Text key='noInterested' style={{ padding: 10 }}>
+                    No Interested Events
+                  </Text>,
+                ];
+              }
+              return stack;
+            })()}
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ width: "48%", alignItems: "center" }}
+          onPress={() => navigation.navigate("Search")}>
+          <Text style={{ fontWeight: "bold", marginBottom: 5 }}>Joined</Text>
+          <View style={UIStyles.stack}>
+            {(() => {
+              let stack = [];
+              for (let index = 0; index < Math.min(joined.length, 4); index++) {
+                if (joined[index]?.image) {
+                  stack.push(
+                    <Image
+                      key={"Joined" + index}
+                      source={{ uri: joined[index]?.image }}
+                      style={{ width: "50%", height: "50%" }}
+                    />
+                  );
+                }
+              }
+
+              if (stack.length === 0) {
+                stack = [
+                  <Text key='noJoined' style={{ padding: 10 }}>
+                    No Joined Events
+                  </Text>,
+                ];
+              }
+              return stack;
+            })()}
+          </View>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  ) : (
+    <ScrollView style={{ width: "100%", height: "100%", marginBottom: 10 }}>
+      <Text style={{ fontWeight: "bold" }}>For you</Text>
+      <ScrollView
+        horizontal={true}
+        contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
+        {storedEvents.map((event) => (
+          <TouchableOpacity
+            key={event.id + "fy"}
+            onPress={() => navigation.navigate("Event", { eventId: event.id })}>
+            <Card
+              style={{ width: 120, height: 180, margin: 5 }}
+              onPress={() =>
+                navigation.navigate("Event", { eventId: event.id })
+              }>
+              <Card.Cover
+                style={{ height: 110 }}
+                source={{ uri: event.image }}
+              />
+              <Card.Content>
+                <Title numberOfLines={1} style={{ fontSize: 14 }}>
+                  {event.name}
+                </Title>
+                <Paragraph numberOfLines={1} style={{ fontSize: 12 }}>
+                  {format(new Date(event.date), "do/MMM")}
+                </Paragraph>
+              </Card.Content>
+            </Card>
           </TouchableOpacity>
         ))}
         {storedEvents.length === 0 && (
-          <Card key={"suggestions"} style={{justifyContent:"center", alignItems:"center", width: 120, height: 180, margin:5,}}>
-            <Card.Content style={{justifyContent:"center"}}>
-              <Title style={{fontSize:14}}>No Events</Title>
+          <Card
+            key={"suggestions"}
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 120,
+              height: 180,
+              margin: 5,
+            }}>
+            <Card.Content style={{ justifyContent: "center" }}>
+              <Title style={{ fontSize: 14 }}>No Events</Title>
             </Card.Content>
           </Card>
         )}
-        </ScrollView>
-        <Text style={{fontWeight:"bold"}}>More suggestions</Text>
-        <ScrollView horizontal={true} contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}> 
-          {shuffled.map((event) => (
-            <TouchableOpacity key={event.id + "ms"} onPress={() => navigation.navigate('Event', { eventId: event.id })}>
-            <Card style={{width: 120, height: 180, margin:5,}} onPress={()=>navigation.navigate("Event", {eventId: event.id })}>
-            <Card.Cover style={{height : 110}} source={{ uri: event.image }} />
-            <Card.Content>
-              <Title numberOfLines={1} style={{fontSize:14}}>{event.name}</Title>
-              <Paragraph numberOfLines={1} style={{fontSize:12}}>{format(new Date(event.date), "do/MMM")}</Paragraph>
-            </Card.Content>
-          </Card>
+      </ScrollView>
+      <Text style={{ fontWeight: "bold" }}>More suggestions</Text>
+      <ScrollView
+        horizontal={true}
+        contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
+        {shuffled.map((event) => (
+          <TouchableOpacity
+            key={event.id + "ms"}
+            onPress={() => navigation.navigate("Event", { eventId: event.id })}>
+            <Card
+              style={{ width: 120, height: 180, margin: 5 }}
+              onPress={() =>
+                navigation.navigate("Event", { eventId: event.id })
+              }>
+              <Card.Cover
+                style={{ height: 110 }}
+                source={{ uri: event.image }}
+              />
+              <Card.Content>
+                <Title numberOfLines={1} style={{ fontSize: 14 }}>
+                  {event.name}
+                </Title>
+                <Paragraph numberOfLines={1} style={{ fontSize: 12 }}>
+                  {format(new Date(event.date), "do/MMM")}
+                </Paragraph>
+              </Card.Content>
+            </Card>
           </TouchableOpacity>
         ))}
         {shuffled.length === 0 && (
-          <Card key={"suggestions"} style={{justifyContent:"center", alignItems:"center", width: 120, height: 180, margin:5,}}>
-            <Card.Content style={{justifyContent:"center"}}>
-              <Title style={{fontSize:14}}>No Events</Title>
+          <Card
+            key={"suggestions"}
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 120,
+              height: 180,
+              margin: 5,
+            }}>
+            <Card.Content style={{ justifyContent: "center" }}>
+              <Title style={{ fontSize: 14 }}>No Events</Title>
             </Card.Content>
           </Card>
         )}
-        </ScrollView>
+      </ScrollView>
     </ScrollView>
-    ))
   );
 };
 
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,30 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { Button, Card, Title, Paragraph, IconButton } from "react-native-paper";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import StoreService from "../services/StoreService";
 import LoginSignup from "./Components/LoginSignup";
 import { format } from "date-fns";
-import UIStyles from './styles';
+import UIStyles from "./styles";
 
 const Profile = () => {
   const navigation = useNavigation();
-  const [user, setUser] = useState('')
-  const [events, setEvents] = useState([])
+  const [user, setUser] = useState("");
+  const [events, setEvents] = useState([]);
 
-  
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const user = await StoreService.getActive();
+  //     setUser(user);
+
+  //     (user.username && (
+  //       setEvents(await StoreService.getUsersEvents(user.username))
+  //     ))
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  const fetchevents = async () => {
+    user?.username &&
+      setEvents(await StoreService.getUsersEvents(user.username));
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchUser = async () => {
+        const user = await StoreService.getActive();
+        setUser(user);
+      };
+
+      fetchUser();
+    }, [])
+  );
+
   useEffect(() => {
-    const fetchData = async () => {
-      const user = await StoreService.getActive();
-      setUser(user);
-
-      (user.username && (
-        setEvents(await StoreService.getUsersEvents(user.username))
-      ))
-    };
-
-    fetchData();
-  }, []);
+    fetchevents();
+  }, [user]);
 
   const reload = () => {
     const fetchUser = async () => {
@@ -33,62 +52,84 @@ const Profile = () => {
     };
 
     fetchUser();
-  }
+  };
 
   return (
     <View
       style={{
         flex: 1,
-        margin : 20
+        margin: 20,
       }}>
-
       {!user ? (
         <View
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-            }}>
-            <LoginSignup />         
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}>
+          <LoginSignup />
         </View>
       ) : (
         <>
-        <Text style={UIStyles.blackTitleText}>{user.username}'s Profile</Text>
-        <Text style={{fontWeight:"bold"}}>Your Events</Text>
-        <ScrollView horizontal={true} contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
-          <TouchableOpacity key={"create"} onPress={() => { navigation.navigate("EventCreate"); }}>
-            <Card style={{width: 120, height: 180, margin:5,}}>
-              <Card.Content style={{alignItems:"center"}}>
-                <Title numberOfLines={1} style={{fontSize:14}}>Create Event</Title>
-                <IconButton icon="plus"/>
-              </Card.Content>
-            </Card>
+          <Text style={UIStyles.blackTitleText}>{user.username}'s Profile</Text>
+          <Text style={{ fontWeight: "bold" }}>Your Events</Text>
+          <ScrollView
+            horizontal={true}
+            contentContainerStyle={{ flexGrow: 1, marginBottom: 10 }}>
+            <TouchableOpacity
+              key={"create"}
+              onPress={() => {
+                navigation.navigate("EventCreate");
+              }}>
+              <Card style={{ width: 120, height: 180, margin: 5 }}>
+                <Card.Content style={{ alignItems: "center" }}>
+                  <Title numberOfLines={1} style={{ fontSize: 14 }}>
+                    Create Event
+                  </Title>
+                  <IconButton icon='plus' />
+                </Card.Content>
+              </Card>
             </TouchableOpacity>
             {events.map((event) => (
-              <TouchableOpacity key={event.id} onPress={() => navigation.navigate('Event', { eventId: event.id })}>
-              <Card style={{width: 120, height: 180, margin:5,}} onPress={()=>navigation.navigate("Event", {eventId: event.id })}>
-              <Card.Cover style={{height : 110}} source={{ uri: event.image }} />
-              <Card.Content>
-                <Title numberOfLines={1} style={{fontSize:14}}>{event.name}</Title>
-                <Paragraph numberOfLines={1} style={{fontSize:12}}>{format(new Date(event.date), "do/MMM")}</Paragraph>
-              </Card.Content>
-            </Card>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      <Button
-        mode='contained'
-        title='Create Event'
-        buttonColor="red"
-        style={{margin:10}}
-        onPress={() => {
-          StoreService.removeActive(),
-          reload()
+              <TouchableOpacity
+                key={event.id}
+                onPress={() =>
+                  navigation.navigate("Event", { eventId: event.id })
+                }>
+                <Card
+                  style={{ width: 120, height: 180, margin: 5 }}
+                  onPress={() =>
+                    navigation.navigate("Event", { eventId: event.id })
+                  }>
+                  <Card.Cover
+                    style={{ height: 110 }}
+                    source={{ uri: event.image }}
+                  />
+                  <Card.Content>
+                    <Title numberOfLines={1} style={{ fontSize: 14 }}>
+                      {event.name}
+                    </Title>
+                    <Paragraph numberOfLines={1} style={{ fontSize: 12 }}>
+                      {format(new Date(event.date), "do/MMM")}
+                    </Paragraph>
+                  </Card.Content>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+          <Button
+            mode='contained'
+            title='Create Event'
+            buttonColor='red'
+            style={{ margin: 10 }}
+            onPress={() => {
+              StoreService.removeActive();
 
-        }}>
-        Log Out
-      </Button>
-      </>
+              reload();
+            }}>
+            Log Out
+          </Button>
+        </>
       )}
     </View>
   );
